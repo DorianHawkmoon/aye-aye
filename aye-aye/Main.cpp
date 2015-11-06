74559c9ecf238fc8fc7eb791b8c1212440a97f7f
Dorian Hawkmoon <dorian.cadenas@gmail.com>
2015-11-07 00:16:07 +0100

    Proyecto configurado

---
+++ aye-aye/Main.cpp
@@ -0,0 +1,15 @@
+/**
+* File: main.cpp
+* Author: dorian
+*
+* Created on 6 de noviembre de 2015
+**/
+
+#include <iostream>
+
+/**
+*
+**/
+int main(int argc, char** argv){
+	std::cout << "Hola mundo, configurando proyecto" << std::endl;
+}
